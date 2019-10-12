aefdb978241a6e88e885fa30e172ce44e6c094e0
xutongbao <1183391880@qq.com>
2019-10-12 09:36:15 +0800

    购物车

---
+++ 10小程序/11书城（包括添加，删除，详情）/miniprogram/components/Nav/Nav.js
@@ -0,0 +1,27 @@
+// components/Nav/Nav.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    navList: Array,
+    currentIndex: Number
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleNav(e) {
+      let { index, id } = e.target.dataset
+      this.triggerEvent('onNav', { index, id })
+    }
+  }
+})
